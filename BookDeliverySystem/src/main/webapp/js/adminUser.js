fae1760ddd6c9f03748ec4ca4a71c73ad03ba762
YoungjinKim-Git <kyj440138@gmail.com>
2023-09-18 17:48:58 +0900

    제목 : 관리지 페이지 추가 날짜 : 2023.09.18 작성자 : 김영진 내용 : 관리자페이지를 위한 adminBook.js adminUser.js 자바스크립트 추가 및 수정

--- BookDeliverySystem/src/main/webapp/js/adminUser.js
+++ BookDeliverySystem/src/main/webapp/js/adminUser.js
@@ -1,35 +1,76 @@
+$(document).ready(function() {
+    $("#userTableButton").click(function() {
+        toggleUserTable();
+    });
+
+    $("#bookTableButton").click(function() {
+        toggleBookTable();
+    });
+
+    $("#rentListButton").click(function() {
+        // 대출관리 버튼에 대한 동작 추가
+    });
+
+    // 초기에 테이블은 숨겨져 있도록 설정
+    hideUserTable();
+    hideBookTable();
+});
+
+function clearTable() {
+    $("#userInfoTable").empty(); // 회원 테이블 내용 삭제
+    $("#bookInfoTable").empty(); // 도서 테이블 내용 삭제
+    $("#bookSearchTable").empty(); // 도서 테이블 내용 삭제
+}
+
+var isUserTableVisible = false;
+
+function toggleUserTable() {
+    isUserTableVisible = !isUserTableVisible;
+    if (isUserTableVisible) {
+        getAllUsers();
+        hideBookTable();
+        hideBookSearchTable();
+        $("#userInfoTable").show();
+    } else {
+        hideUserTable();
+    }
+}
+
 function getAllUsers() {
-	var userList = $("<table id='userList'>").append(
-			$("<tr>").append(
-				"<th>회원번호</th>",
-		        "<th>회원이메일</th>",
-		        "<th>회원이름</th>",
-		        "<th>전화번호</th>",
-		        "<th>회원생일</th>",
-		        "<th>가입일</th>",
-		        "<th>성별</th>"	
-			)
-	);		
 	
-    $.get("./getAllUser.do", function(getAllusers) { 
+    $("#bookInfoTable").hide();
+    $("#userInfoTable").show();
+    var userList = $("<table id='userList'>").append(
+        $("<tr>").append(
+            "<th>회원번호</th>",
+            "<th>회원이메일</th>",
+            "<th>회원이름</th>",
+            "<th>전화번호</th>",
+            "<th>회원생일</th>",
+            "<th>가입일</th>",
+            "<th>성별</th>"
+        )
+    );
+
+    $.get("./getAllUser.do", function(getAllusers) {
         $.each(getAllusers, function(index, user) {
             userList.append(
-            	$("<tr>").append(
-            	"<td>"+ user.user_seq + "</td>",
-            	"<td>"+ user.user_email + "</td>",
-            	"<td>"+ user.user_name + "</td>",
-            	"<td>"+ user.user_phone + "</td>",
-            	"<td>"+ user.user_birth + "</td>",
-            	"<td>"+ user.joindate + "</td>",
-            	"<td>"+ user.user_gender + "</td>"
-            	)
+                $("<tr>").append(
+                    "<td>" + user.user_seq + "</td>",
+                    "<td>" + user.user_email + "</td>",
+                    "<td>" + user.user_name + "</td>",
+                    "<td>" + user.user_phone + "</td>",
+                    "<td>" + user.user_birth + "</td>",
+                    "<td>" + user.joindate + "</td>",
+                    "<td>" + user.user_gender + "</td>"
+                )
             );
         });
     });
-    $("#userList").remove();
-    $("#userInfoTable").append(userList);
-    
-    if ($("#bookinfoTable").length) {
-        $("#bookinfoTable").remove();
-    }
+
+    $("#userInfoTable").html(userList);
+}
+
+function hideUserTable() {
+    $("#userInfoTable").empty();
 }
